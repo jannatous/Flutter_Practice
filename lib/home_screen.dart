d5f8764fcad9bd6117711d43afa58ab53be4bc80
jannat <jannatous121@gmail.com>
2023-10-05 21:32:37 +0600

    Calculator(add,sub,mul,div,mod)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,25 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/home_screen.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:HomeScreen(),
-
-      ); // This trailing comma makes auto-formatting nicer for build methods.
-  }
-}
-
-
-//import 'package:flutter/material.dart';
 class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
   @override
